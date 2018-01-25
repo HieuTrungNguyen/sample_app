ddf16668b6ef172b7883c0f11a884d7b45f8a99d
Hieu Trung Nguyen <nguyentrunghieu3103@gmail.com>
2018-01-25 16:03:47 +0700

    account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create! name:  "Hieu Trung Nguyen",
              email: "nguyentrunghieu3103@gmail.com",
              password: "Ng0kyeuem39",
              password_confirmation: "Ng0kyeuem39",
-             admin: true
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now
 
 99.times do |n|
   name  = "User #{n+1}"
@@ -11,5 +13,7 @@ User.create! name:  "Hieu Trung Nguyen",
   User.create!(name:  name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
